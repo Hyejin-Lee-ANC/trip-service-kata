2266f8174e1ee90413fc264cf30af4e1582a6bc8
JoeBew42 <joebew42@gmail.com>
2019-02-17 20:23:34 +0100

    introducing seams and collaborators

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,32 +1,44 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
-	
+    public static final ArrayList<Trip> NO_TRIPS = new ArrayList<>();
+
+    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        return getTripsByUser(user, getLoggedInUser());
+    }
+
+    protected List<Trip> getTripsByUser(User user, User loggedUser) {
+        checkIfTheUserIsLoggedIn(loggedUser);
+
+        if (user.friendsOf(loggedUser)) {
+            return tripsOf(user);
+        }
+
+        return NO_TRIPS;
+    }
+
+    protected void checkIfTheUserIsLoggedIn(User loggedUser) {
+        if (loggedUser == null) {
+            throw new UserNotLoggedInException();
+        }
+    }
+
+    // Seams
+
+    protected List<Trip> tripsOf(User user) {
+        return TripDAO.findTripsByUser(user);
+    }
+
+    protected User getLoggedInUser() {
+        return UserSession.getInstance().getLoggedUser();
+    }
+
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,80 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 public class TripServiceTest {
-	
+
+    public static final User ANY_USER = null;
+    public static final User LOGGED_IN_USER = new User();
+    public static final User NOT_LOGGED_IN_USER = null;
+    public static final Trip A_TRIP = new Trip();
+
+    @Test(expected = UserNotLoggedInException.class)
+    public void throws_an_exception_when_user_is_not_logged_in() {
+        TripService service = new TripServiceFor(NOT_LOGGED_IN_USER);
+
+        service.getTripsByUser(ANY_USER);
+    }
+
+    @Test
+    public void returns_no_trips_for_user_with_no_friends() {
+        TripService service = new TripServiceFor(LOGGED_IN_USER);
+        User userWithNoFriends = new User();
+
+        List<Trip> trips = service.getTripsByUser(userWithNoFriends);
+
+        assertTrue(trips.isEmpty());
+    }
+
+    @Test
+    public void no_trips_when_user_is_not_a_friend_of_the_logged_in_user() {
+        TripService service = new TripServiceFor(LOGGED_IN_USER);
+
+        User userWithFriends = new User();
+        userWithFriends.addFriend(new User());
+
+        List<Trip> trips = service.getTripsByUser(userWithFriends);
+
+        assertTrue(trips.isEmpty());
+    }
+
+    @Test
+    public void returns_the_trips_of_the_user_when_friends_of_the_logged_in_user() {
+        TripService service = new TripServiceFor(LOGGED_IN_USER);
+
+        User userWithFriends = new User();
+        userWithFriends.addFriend(LOGGED_IN_USER);
+        userWithFriends.addTrip(A_TRIP);
+
+        List<Trip> trips = service.getTripsByUser(userWithFriends);
+
+        assertTrue(trips.contains(A_TRIP));
+    }
+
+    // Override a class just for testing purposes
+
+    private class TripServiceFor extends TripService {
+
+        private User loggedInUser;
+
+        public TripServiceFor(User loggedInUser) {
+            this.loggedInUser = loggedInUser;
+        }
+
+        @Override
+        protected List<Trip> tripsOf(User user) {
+            return user.trips();
+        }
+
+        @Override
+        protected User getLoggedInUser() {
+            return loggedInUser;
+        }
+    }
 }
